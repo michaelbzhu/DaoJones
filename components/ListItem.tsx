f277e04c82c0dde4a912fbe4f6327adc4b54c883
Michael Zhu <michael.zhu@retool.com>
2023-03-02 18:56:04 -0800

    downgrade next to 12

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -9,7 +9,9 @@ type Props = {
 
 const ListItem = ({ data }: Props) => (
   <Link href="/users/[id]" as={`/users/${data.id}`}>
-    {data.id}: {data.name}
+    <>
+      {data.id}: {data.name}
+    </>
   </Link>
 );
 
