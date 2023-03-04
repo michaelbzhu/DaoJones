51d0a1e87673cfc32c4f6b44a55368de08bb230a
Michael Zhu <michael.zhu@retool.com>
2023-03-03 22:55:27 -0800

    WIP: mocked data; need to figure out how to render react table stuff

--- src/pages/GovernorTable.tsx
+++ src/pages/GovernorTable.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { createColumnHelper, useReactTable } from "@tanstack/react-table";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import client from "./api/tally/apollo-client";
 import { GetServerSideProps } from "next";
 import { gql } from "@apollo/client";
@@ -9,13 +14,74 @@ type GovernorData = {
   name: string;
   macroScore: number;
 };
+const columnHelper = createColumnHelper<GovernorData>();
 
 const GovernorTable = (governors: GovernorData[]) => {
-  const columnHelper = createColumnHelper<GovernorData>();
-  return <></>;
+  console.log({ governors });
+  const columns = [
+    columnHelper.accessor((row) => `${row.name}`, {
+      header: () => "DAO",
+      id: "name",
+      cell: (info) => <i>{info.getValue()}</i>,
+    }),
+    columnHelper.accessor((row) => `${row.macroScore}`, {
+      header: () => "Credit Score",
+      id: "macroScore",
+      cell: (info) => info.renderValue(),
+    }),
+  ];
+
+  const table = useReactTable({
+    data: governors,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="p-2">
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  // mocked data - the query below returns this object
+  return {
+    props: {
+      governors: [
+        { name: "veggieDAO", macroScore: 596.3076923076923 },
+        { name: "Nouns Dao", macroScore: 608.2 },
+      ],
+    },
+  };
   const { data } = await client.query({
     query: gql`
       query Governors(
@@ -54,7 +120,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   const governorPromises = [];
 
-  const governors: { [name: string]: unknown } = {};
+  const governors: { [name: string]: number } = {};
 
   data.governors.forEach(({ name, delegates }) => {
     console.log({
@@ -69,10 +135,13 @@ export const getServerSideProps: GetServerSideProps = async () => {
           }),
         })
         .json()
-        .then((response) => {
+        .then(({ walletInfos }) => {
           console.log("-------------");
-          console.log({ name, response });
-          governors[name] = response;
+          console.log({ name, walletInfos });
+          governors[name] =
+            Object.keys(walletInfos).reduce<number>((sum, address) => {
+              return sum + Number(walletInfos[address].score);
+            }, 0) / Object.keys(walletInfos).length;
         })
         .catch((err) => {
           console.log("error----------------------", err);
@@ -83,9 +152,15 @@ export const getServerSideProps: GetServerSideProps = async () => {
   await Promise.all(governorPromises);
 
   console.log("governors", governors);
+  const governorData: GovernorData[] = Object.keys(governors).map((dao) => ({
+    name: dao,
+    macroScore: governors[dao],
+  }));
+
+  console.log("governorData", governorData);
 
   return {
-    props: { data },
+    props: { governors: governorData },
   };
 };
 
