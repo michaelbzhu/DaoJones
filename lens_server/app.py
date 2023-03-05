909107683d427becbdd408582fafa68c1ef94e34
Rishabh Krishnan <rishabh.krishnan@berkeley.edu>
2023-03-04 20:02:49 -0700

    added cors support

--- lens_server/app.py
+++ lens_server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from google.cloud import bigquery
 import pdb
 import json
 app = Flask(__name__)
+CORS(app)
 client = bigquery.Client()
 initial_query = """
     SELECT (address, follow_profile_id)
