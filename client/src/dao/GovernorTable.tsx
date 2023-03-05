916cf2842693cba3263848600597bd1eca919e5c
Michael Zhu <michael.zhu@retool.com>
2023-03-04 20:11:56 -0800

    move react table into seperate component

--- client/src/dao/GovernorTable.tsx
+++ client/src/dao/GovernorTable.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { useTable } from 'react-table'
 import { getGovernors } from '../tally/getGovernors'
 import { scheduleAndRequestSpectralScores } from '../spectral/scheduleAndRequestSpectralScores'
 import { daoImageMap } from '../utils/daoImageMap'
@@ -67,61 +66,7 @@ function BasicTable({ data }: { data: GovernorRowData[] }) {
   )
 }
 
-function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
-      columns,
-      data,
-    })
-
-  // Render the UI for your table
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              })}
-            </tr>
-          )
-        })}{' '}
-      </tbody>
-    </table>
-  )
-}
-
 function GovernorTable() {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Name',
-        accessor: 'name',
-      },
-      {
-        Header: 'Address',
-        accessor: 'address',
-      },
-      {
-        Header: 'Score',
-        accessor: 'score',
-      },
-    ],
-    []
-  )
-
   const [tableData, setTableData] = React.useState(null)
 
   const data = React.useMemo(() => tableData, [tableData])
