0c1f408e8dd94eb2669762df6edd85401354682a
Daniel Tao <danieltaox@gmail.com>
2023-03-04 20:00:51 -0700

    feat: migrate from webpack to vite

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -4,10 +4,20 @@ import { Link, Outlet } from "react-router-dom";
 function Layout() {
   return (
     <div id="App">
+      <div className="navbar">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">Trident</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+          <Link className="text-xl" to="/lens">DAOs</Link>
+          <Link className="text-xl" to="/lens">Lens</Link>
+          <Link className="text-xl" to="/graph">Graph</Link>
+          </ul>
+        </div>
+      </div>
+
       <div className="container">
-        <Link className="text-xl" to="/lens">LENS LINK</Link>
-        <Link className="text-xl" to="/graph">GRAPH LINK</Link>
-        <hr />
         <Outlet />
       </div>
     </div>
