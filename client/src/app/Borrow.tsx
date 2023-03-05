8069bd83b4c5d0405159e01270ead26716fa9403
Michael Zhu <michael.zhu@retool.com>
2023-03-05 02:47:24 -0800

    setup ui for borrow

--- client/src/app/Borrow.tsx
+++ client/src/app/Borrow.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAccount, useBalance } from 'wagmi'
 
 const Borrow = () => {
+  const { address } = useAccount()
+  const { data, isError, isLoading } = useBalance({ address })
+
+  const [outstanding, setOutstanding] = useState(20.5)
+
   return (
-    <div>
-      <div>Current interest rate for your Dao</div>
-      <div>OUtstanding balance</div>
-      <div>text box for how much to borrow</div>
-      <button>submit</button>
+    <div className="w-full">
+      {isLoading ? (
+        <div>Fetching balance…</div>
+      ) : (
+        <div className="stats shadow">
+          <div className="stat">
+            <div className="stat-title">Interest Rate</div>
+            <div className="stat-value">
+              2.50% {/* replace this with algo */}
+            </div>
+          </div>
+          <div className="stat">
+            <div className="stat-title">Outstanding Balance</div>
+            <div className="stat-value">{`${outstanding} ETH`}</div>
+          </div>
+        </div>
+      )}
+      {isError ? <div>Error fetching balance</div> : null}
+      <div className="mt-8 py-4">
+        <input
+          type="number"
+          placeholder="Amount"
+          className=" ≈ input-bordered input-secondary input w-full max-w-xs"
+        />
+      </div>
+
+      {outstanding !== 0 ? (
+        <button className="btn-secondary btn m-4">Pay</button>
+      ) : (
+        <button className="btn-secondary btn m-4">Borrow</button>
+      )}
     </div>
   )
 }
