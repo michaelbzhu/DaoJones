a16fb3bda4913cdf609e9c9dc8f8aeeb76d6c515
Michael Zhu <michael.zhu@retool.com>
2023-03-05 02:29:04 -0800

    add withdraw and lend button

--- client/src/app/Lend.tsx
+++ client/src/app/Lend.tsx
@@ -5,28 +5,35 @@ const Lend = () => {
   const { address } = useAccount()
   const { data, isError, isLoading } = useBalance({ address })
   return (
-    <div className="w-80">
+    <div className="w-full">
       {isLoading ? (
         <div>Fetching balance…</div>
       ) : (
         <div className="stats shadow">
           <div className="stat">
-            <div className="stat-title">Account Balance</div>
+            <div className="stat-title">Account balance</div>
             <div className="stat-value">
               {data.formatted + ' ' + data.symbol}
             </div>
           </div>
+          <div className="stat">
+            <div className="stat-title">Lending @ 2.00%</div>
+            <div className="stat-value">1.123 ETH</div>
+          </div>
         </div>
       )}
       {isError ? <div>Error fetching balance</div> : null}
-      <div className="py-4">
+      <div className="mt-8 py-4">
         <input
           type="number"
           placeholder="Amount"
           className=" ≈ input-bordered input-secondary input w-full max-w-xs"
         />
       </div>
-      <button className="btn-secondary btn">Lend</button>
+      <div className="flex justify-center">
+        <button className="btn-secondary btn m-4">Lend</button>
+        <button className="btn-secondary btn m-4">Withdraw</button>
+      </div>
     </div>
   )
 }
