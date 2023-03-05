54156905c111e915e2b0cce0d46d2039f23f285b
Michael Zhu <michael.zhu@retool.com>
2023-03-04 23:55:59 -0800

    get delegate spectral info for whitelisted daos

--- client/src/spectral/getSpectralScores.ts
+++ client/src/spectral/getSpectralScores.ts
@@ -1,4 +1,4 @@
-import { getAuthedSpectralApi } from "./getAuthedSpectralApi";
+import { getAuthedSpectralApi } from './getAuthedSpectralApi'
 
 // spectral api return object
 // walletInfos: {
@@ -14,14 +14,14 @@ import { getAuthedSpectralApi } from "./getAuthedSpectralApi";
 //   }
 
 type SpectralWalletInfoWithScore = {
-  probability_of_liquidation: string;
-  risk_level: string;
-  score: string;
-  score_ingredients: object;
-  score_timestamp: string;
-  status: string;
-  wallet_address: string;
-};
+  probability_of_liquidation: string
+  risk_level: string
+  score: string
+  score_ingredients: object
+  score_timestamp: string
+  status: string
+  wallet_address: string
+}
 
 // type SpectralWalletInfoWithoutScore = {
 //   status: string;
@@ -36,12 +36,12 @@ type SpectralWalletInfoWithScore = {
 export const getSpectralScores = async ({
   wallets,
 }: {
-  wallets: string[];
+  wallets: string[]
 }): Promise<{ [wallet: string]: SpectralWalletInfoWithScore }> => {
-  const spectralPromises: unknown[] = [];
-  const walletInfos: { [wallet: string]: SpectralWalletInfoWithScore } = {};
+  const spectralPromises: unknown[] = []
+  const walletInfos: { [wallet: string]: SpectralWalletInfoWithScore } = {}
 
-  const api = getAuthedSpectralApi();
+  const api = getAuthedSpectralApi()
 
   // make get requests to spectral
   wallets.forEach((wallet) => {
@@ -50,17 +50,20 @@ export const getSpectralScores = async ({
         .get(`https://api.spectral.finance/api/v1/addresses/${wallet}`)
         .json()
         .then((walletInfo) => {
-          //   console.log("spectral/index single wallet -------------------", {
-          //     wallet,
-          //     walletInfo,
-          //   });
-          if ((walletInfo as object).hasOwnProperty("score")) {
-            walletInfos[wallet] = walletInfo as SpectralWalletInfoWithScore;
+          console.log('spectral get single wallet -------------------', {
+            wallet,
+            walletInfo,
+          })
+          if ((walletInfo as object).hasOwnProperty('score')) {
+            walletInfos[wallet] = walletInfo as SpectralWalletInfoWithScore
           }
         })
-    );
-  });
+        .catch((err) => {
+          console.log('spectral get single wallet -------------------', err)
+        })
+    )
+  })
 
-  await Promise.all(spectralPromises);
-  return walletInfos;
-};
+  await Promise.all(spectralPromises)
+  return walletInfos
+}
